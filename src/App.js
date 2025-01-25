c811bd5fa454b35cd99baa6427647d81d8512822
Anthony Zurita <anthonyy.zurita@gmail.com>
2025-01-25 17:57:52 -0500

    Work in progress for the home page saving progree at this point

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import LoginButton from "./components/LoginButton";
 import LogoutButton from "./components/LogoutButton";
 import Profile from "./components/Profile";
+import LightNavbar from "./components/Navbar";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <main className="column">
+      <LightNavbar />
       <h1>Auth0 Login</h1>
       <LoginButton />
       <LogoutButton />
---
+++ src/components/Navbar.js
@@ -0,0 +1,20 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+function LightNavbar() {
+  return (
+      <Navbar bg="light" data-bs-theme="light">
+        <Container>
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Learn</Nav.Link>
+            <Nav.Link href="#pricing">Tutors</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+  );
+}
+
+export default LightNavbar;
